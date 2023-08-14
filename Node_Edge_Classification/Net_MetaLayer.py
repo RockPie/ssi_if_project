69e3dcbce2bd89a8dd647d2bc7954512c353b09c
Shihai Jia <shihai.jia@outlook.com>
2023-08-14 11:59:42 -0700

    Batch info added

--- Node_Edge_Classification/Net_MetaLayer.py
+++ Node_Edge_Classification/Net_MetaLayer.py
@@ -7,7 +7,7 @@ class EdgeModel(torch.nn.Module):
     def __init__(self, edge_feature_size, node_feature_size,layer_size):
         super().__init__()
         net_layers = []
-        in_size = edge_feature_size + 2*node_feature_size
+        in_size = edge_feature_size + 2*node_feature_size + 1
 
         net_layers.append(torch.nn.Linear(in_size, layer_size))
         net_layers.append(torch.nn.BatchNorm1d(layer_size))
@@ -26,10 +26,17 @@ class EdgeModel(torch.nn.Module):
         # edge_attr: [E, F_e]
         # u: [B, F_u], where B is the number of graphs.
         # batch: [E] with max entry B - 1.
-        out = torch.cat([src, dst, edge_attr], 1)
+        # print("src size: ", src.size())
+        # print("dst size: ", dst.size())
+        # print("edge_attr size: ", edge_attr.size())
+        # print("batch size: ", batch.size())
+        # make batch two dimension
+        batch = batch.unsqueeze(1)
+        out = torch.cat([src, dst, edge_attr, batch], 1)
         out = self.model(out)
         out = self.linear(out)
         out = torch.nn.ReLU()(out)
+        # out = F.dropout(out, training=self.training)
 
         return out
 
@@ -37,32 +44,36 @@ class NodeModel(torch.nn.Module):
     def __init__(self, edge_feature_size, node_feature_size, layer_size):
         super().__init__()
         self.node_mlp_1 = torch.nn.Sequential(
-            torch.nn.Linear(node_feature_size + edge_feature_size, layer_size),
+            torch.nn.Linear(node_feature_size + edge_feature_size + 1, layer_size),
             torch.nn.BatchNorm1d(layer_size),
             torch.nn.ReLU()
         )
         self.node_mlp_2 = torch.nn.Sequential(
-            torch.nn.Linear(node_feature_size + layer_size, node_feature_size + layer_size),
-            torch.nn.BatchNorm1d(layer_size + node_feature_size),
+            torch.nn.Linear(node_feature_size + layer_size + 1, node_feature_size + layer_size + 1),
+            torch.nn.BatchNorm1d(layer_size + node_feature_size + 1),
             torch.nn.ReLU()
         )
 
-        self.linear = torch.nn.Linear(layer_size + node_feature_size, node_feature_size)
+        self.linear = torch.nn.Linear(layer_size + node_feature_size + 1, node_feature_size)
     def forward(self, x, edge_index, edge_attr, u, batch):
         # x: [N, F_x], where N is the number of nodes.
         # edge_index: [2, E] with max entry N - 1.
         # edge_attr: [E, F_e]
         # u: [B, F_u]
         # batch: [N] with max entry B - 1.
+        # print("node number: ", x.size(0))
+        # print("edge number: ", edge_index.size(1))
         row, col = edge_index
-        out = torch.cat([x[row], edge_attr], dim=1)
+        batch = batch.unsqueeze(1)
+        out = torch.cat([x[row], batch[row], edge_attr], dim=1)
         out = self.node_mlp_1(out)
         out = scatter(out, col, dim=0, dim_size=x.size(0),
                       reduce='mean')
-        out = torch.cat([x, out], dim=1)
+        out = torch.cat([x, batch, out], dim=1)
         out = self.node_mlp_2(out)
         out = self.linear(out)
         out = torch.nn.ReLU()(out)
+        # out = F.dropout(out, training=self.training)
         return out
 
 class GlobalModel(torch.nn.Module):
@@ -112,26 +123,44 @@ class SJN_Meta(torch.nn.Module):
         edge_index  = edge_index.to(self.device)
         edge_attr   = edge_attr.to(self.device)
         # u           = u.to(self.device)
-        # batch       = batch.to(self.device)
+        batch       = batch.to(self.device)
+        # print("x datatype: ", x.dtype)
+        # print("edge_index datatype: ", edge_index.dtype)
+        # print("edge_attr datatype: ", edge_attr.dtype)
+        # print("batch datatype: ", batch.dtype)
 
         x, edge_attr, u = self.metalayer1(x, edge_index, edge_attr, u, batch)
         x, edge_attr, u = self.metalayer2(x, edge_index, edge_attr, u, batch)
-        # x, edge_attr, u = self.metalayer3(x, edge_index, edge_attr, u, batch)
+        x, edge_attr, u = self.metalayer3(x, edge_index, edge_attr, u, batch)
+        
+        x = F.dropout(x, p=0.05, training=self.training)
+        edge_attr = F.dropout(edge_attr, p=0.05, training=self.training)
 
         # row, col = edge_index
         # hybrid_feature = torch.cat([x[row], x[col], edge_attr], dim=1)
         # hybrid_feature = torch.nn.Linear(51, 256)(hybrid_feature)
+        # hybrid_feature = torch.nn.BatchNorm1d(256)(hybrid_feature)
         # hybrid_feature = torch.nn.ReLU()(hybrid_feature)
         # hybrid_feature = torch.nn.Linear(256, 256)(hybrid_feature)
+        # hybrid_feature = torch.nn.BatchNorm1d(256)(hybrid_feature)
         # hybrid_feature = torch.nn.ReLU()(hybrid_feature)
 
         # edge_label_pred = torch.nn.Linear(256, 1)(hybrid_feature)
         # edge_label_pred = F.sigmoid(edge_label_pred)
-        x = self.x_linear(x)
-        y_pred = F.sigmoid(x)
 
-        edge_attr = F.dropout(edge_attr, p=0.1, training=self.training)
+        x = self.x_linear(x)
+        # print(x)
+        y_pred = F.sigmoid(x)
+        # softmax
+        # y_pred = F.softmax(x, dim=1)
         edge_attr = self.edge_linear(edge_attr)
         edge_label_pred = F.sigmoid(edge_attr)
+        # edge_label_pred = F.softmax(edge_attr, dim=1)
 
         return y_pred, edge_label_pred
+    
+def DrielsmaLoss(model_outputs, labels):
+    _batch_size = model_outputs.size()[0]
+    _loss_array = labels * torch.log(model_outputs) + (1 - labels) * torch.log(1 - model_outputs)
+    _loss = -torch.sum(_loss_array) / _batch_size
+    return _loss
--- Node_Edge_Classification/Node_Edge_Classification.py
+++ Node_Edge_Classification/Node_Edge_Classification.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import h5py as h5
 import time
+import json
 import Net_Tri_EdgeCov
 import Net_MetaLayer
 from termcolor import colored
@@ -28,9 +29,12 @@ if __name__ == '__main__':
     _=np.random.seed(SEED)
     _=torch.manual_seed(SEED)
 
+    epoch_num = 5
     batch_size = 256
-    training_event_size = 110409
-    test_event_size = 5000
+    enable_save = True
+    # training_event_size = 110409
+    training_event_size = 40000
+    test_event_size = 4000
 
     train_datapath = "./Node_Edge_Classification/data/if-graph-train.h5"
 
@@ -43,9 +47,10 @@ if __name__ == '__main__':
     # use only a slice of the training data
     train_data_slice = []
     print(colored('Slicing training data ...', 'green'))
-    for i in range(0, training_event_size):
-        train_data_slice.append(train_data[i])
-    print('Size of training data: ', len(train_data))
+    if training_event_size < 110409:
+        for i in range(0, training_event_size):
+            train_data_slice.append(train_data[i])
+        print('Size of training data: ', len(train_data))
 
     test_datapath = "./Node_Edge_Classification/data/if-graph-test.h5"
     print(colored('Loading test data from: ', 'green'), test_datapath)
@@ -68,12 +73,20 @@ if __name__ == '__main__':
 
     print(colored('Creating data loaders ...', 'cyan'))
 
-    train_loader = GraphDataLoader(
-        train_data_slice,
-        num_workers = 0,
-        shuffle     = True,
-        batch_size  = batch_size
-    )
+    if training_event_size < 110409:
+        train_loader = GraphDataLoader(
+            train_data_slice,
+            num_workers = 0,
+            shuffle     = True,
+            batch_size  = batch_size
+        )
+    else:
+        train_loader = GraphDataLoader(
+            train_data,
+            num_workers = 0,
+            shuffle     = True,
+            batch_size  = batch_size
+        )
 
     test_loader = GraphDataLoader(
         test_data_slice,
@@ -96,21 +109,22 @@ if __name__ == '__main__':
     u_placeholder     = None
 
     print(colored('Testing model ...', 'green'))
-    try_pred = model(
-        x = train_data[0].x, 
-        edge_index = train_data[0].edge_index, 
-        edge_attr  = train_data[0].edge_attr, 
-        batch = batch_placeholder,
-        u = u_placeholder)
+    # try_pred = model(
+    #     x = train_data[0].x, 
+    #     edge_index = train_data[0].edge_index, 
+    #     edge_attr  = train_data[0].edge_attr, 
+    #     batch = batch_placeholder,
+    #     u = u_placeholder)
     # print(try_pred)
 
 
     print(colored('Defining loss and optimizer ...', 'green'))
     loss_func = torch.nn.BCELoss()
+    # loss_func =  Net_MetaLayer.DrielsmaLoss()
     loss_func.to(device)
     loss_edge_ratio = 0.5
     loss_node_ratio = 1 - loss_edge_ratio
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0025)
     # optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
     model.train()
 
@@ -118,10 +132,12 @@ if __name__ == '__main__':
     loss_array_edge = []
     test_acc_node = []
     test_acc_edge = []
-    epoch_num = 20
+
 
     print(colored('Training ...', 'cyan', attrs=['bold']))
+    # ! training loop
     for epoch in range(epoch_num):
+        epoch_start_time = time.time()
         batch_cnt = 0
         for batch in train_loader:
             batch_cnt += 1
@@ -131,55 +147,103 @@ if __name__ == '__main__':
                 x = batch.x,
                 edge_index = batch.edge_index,
                 edge_attr  = batch.edge_attr,
-                batch = batch_placeholder,
+                batch = batch.batch,
                 u = u_placeholder
             )
-
             node_labels = batch.y.reshape(len(batch.y), 1).float()
             node_labels = node_labels.to(device)
             loss_node   = loss_func(pred_y, node_labels)
+            # loss_node = Net_MetaLayer.DrielsmaLoss(pred_y, node_labels)
 
             edge_labels = batch.edge_label.reshape(len(batch.edge_label), 1).float()
             edge_labels = edge_labels.to(device)
             loss_edge   = loss_func(pred_edge_label, edge_labels)
-            loss = loss_node_ratio * loss_node + loss_edge_ratio * loss_edge
+            # loss_edge = Net_MetaLayer.DrielsmaLoss(pred_edge_label, edge_labels)
+            loss = loss_node + loss_edge
+            # loss = loss_node_ratio * loss_node + loss_edge_ratio * loss_edge
             loss.backward()
             optimizer.step()
             loss_array_node.append(loss_node.item())
             loss_array_edge.append(loss_edge.item())
             end_time = time.time()
             loss_2p3_str = str(loss_edge.item())[:5]
-            time_2p2_str = str((end_time - start_time)*1000)[:4]
+            batch_time = end_time - start_time
+            batch_time = np.round(batch_time*1000, 1)
+            time_2p2_str = str(batch_time)
             print('[ Epoch: ', epoch, ' Edge Loss: ', loss_2p3_str, ' Time[ms]: ', time_2p2_str, 'batch:' , batch_cnt, '/', train_batch_num, ' ]',end='\r')
 
         local_acc_node = []
         local_acc_edge = []
+        local_last_pred_y_true = []
+        local_last_pred_y_false = []
+        local_last_pred_edge_true = []
+        local_last_pred_edge_false = []
+        local_last_pred_edge_label = []
+        # ! Start testing
         for batch in test_loader:
             pred_y, pred_edge_label = model(
                 x = batch.x,
                 edge_index = batch.edge_index,
                 edge_attr  = batch.edge_attr,
-                batch = batch_placeholder,
+                batch = batch.batch,
                 u = u_placeholder
             )
+            
             pred_y = pred_y.detach().to('cpu').numpy()
+            # pred_y = pred_y[:, 1]
+            real_y = batch.y.reshape(len(batch.y), 1).numpy()
+            for i in range(len(pred_y)):
+                if real_y[i].item() == 1:
+                    local_last_pred_y_true.append(pred_y[i].item())
+                else:
+                    local_last_pred_y_false.append(pred_y[i].item())
             pred_y = np.round(pred_y)
 
             pred_edge_label = pred_edge_label.detach().to('cpu').numpy()
+            # pred_edge_label = pred_edge_label[:, 1]
+            real_edge_label = batch.edge_label.reshape(len(batch.edge_label), 1).numpy()
+            for i in range(len(pred_edge_label)):
+                if real_edge_label[i].item() == 1:
+                    local_last_pred_edge_true.append(pred_edge_label[i].item())
+                else:
+                    local_last_pred_edge_false.append(pred_edge_label[i].item())
             pred_edge_label = np.round(pred_edge_label)
             # accuracy is the number of correct predictions divided by the number of predictions
-            acc_y = np.sum(pred_y == batch.y.reshape(len(batch.y), 1).numpy()) / len(batch.y)
+            acc_y = np.sum(pred_y == real_y) / len(batch.y)
             acc_edge = np.sum(pred_edge_label == batch.edge_label.reshape(len(batch.edge_label), 1).numpy()) / len(batch.edge_label)
+
             local_acc_node.append(acc_y)
             local_acc_edge.append(acc_edge)
         test_acc_edge.append(np.mean(local_acc_edge))
         test_acc_node.append(np.mean(local_acc_node))
-        print('\n-        ', epoch, ' Node accuracy: ', str(np.mean(local_acc_node))[:5], ' Edge accuracy: ', str(np.mean(local_acc_edge))[:5])
+        epoch_time = (time.time() - epoch_start_time)
+        epoch_time = np.round(epoch_time, 2)
+        print('\n-        ', epoch, ' Node accuracy: ', str(np.mean(local_acc_node))[:5], ' Edge accuracy: ', str(np.mean(local_acc_edge))[:5], ' Time[sec]: ', str(epoch_time))
 
     print('Overallest node accuracy: ', np.mean(test_acc_node))
     print('Overallest edge accuracy: ', np.mean(test_acc_edge))
-            
-    figure_train = plt.figure(figsize=(10, 5), dpi=1000)
+    local_time = time.localtime(time.time())
+    # save model
+    model_name = './Node_Edge_Classification/temp/model' + str(local_time.tm_year) + str(local_time.tm_mon) + str(local_time.tm_mday) + str(local_time.tm_hour) + str(local_time.tm_min) + str(local_time.tm_sec) + '.pt'
+    if enable_save:
+        torch.save(model.state_dict(), model_name)
+    # save loss into json
+    loss_dict = {
+        'loss_node': loss_array_node,
+        'loss_edge': loss_array_edge,
+        'test_acc_node': test_acc_node,
+        'test_acc_edge': test_acc_edge
+    }
+    json_name = './Node_Edge_Classification/temp/loss' + str(local_time.tm_year) + str(local_time.tm_mon) + str(local_time.tm_mday) + str(local_time.tm_hour) + str(local_time.tm_min) + str(local_time.tm_sec) + '.json'
+    if enable_save:
+        with open(json_name, 'w') as f:
+            json.dump(loss_dict, f)
+    
+    # * Create loss figure
+    if enable_save:
+        figure_train = plt.figure(figsize=(10, 5), dpi=1000)
+    else:
+        figure_train = plt.figure(figsize=(10, 5))
     plt.plot(loss_array_node, label='Node loss', color='#04B5BBEE')
     plt.plot(loss_array_edge, label='Edge loss', color='#DF0345EE')
     figure_info = 'Training with ' + str(epoch_num) + ' epochs, ' + str(training_event_size) + ' events per epoch, ' + str(batch_size) + ' events per batch'
@@ -188,6 +252,7 @@ if __name__ == '__main__':
     plt.ylabel('Loss')
     plt.legend(loc='lower left')
     # sub y axis
+    ax1 = plt.gca()
     ax2 = plt.twinx()
     # unify x axis
     x_acc = np.arange(0, len(test_acc_node), 1)
@@ -200,9 +265,51 @@ if __name__ == '__main__':
     # set x axis range
     plt.xlim(0, len(loss_array_node))
 
-    plt.ylim(0, max(loss_array_node) * 1.1)
+    ax1.set_ylim(0, 0.8)
     ax2.set_ylim(0.8, 1)
     plt.grid()
-    local_time = time.localtime(time.time())
-    plt.savefig('./Node_Edge_Classification/pics/res' + str(local_time.tm_year) + str(local_time.tm_mon) + str(local_time.tm_mday) + str(local_time.tm_hour) + str(local_time.tm_min) + str(local_time.tm_sec) + '.png')
+    
+    if enable_save:
+        plt.savefig('./Node_Edge_Classification/pics/res' + str(local_time.tm_year) + str(local_time.tm_mon) + str(local_time.tm_mday) + str(local_time.tm_hour) + str(local_time.tm_min) + str(local_time.tm_sec) + '.png')
+    else:
+        plt.show()
+
+    # * Create node hist figure
+    if enable_save:
+        figure_node_hist = plt.figure(figsize=(8, 8), dpi=600)
+    else:
+        figure_node_hist = plt.figure(figsize=(8, 8))
+
+    print('False node value number: ', len(local_last_pred_y_false))
+    print('True node value number: ', len(local_last_pred_y_true))
+    print('False edge value number: ', len(local_last_pred_edge_false))
+    print('True edge value number: ', len(local_last_pred_edge_true))
+
+    print('Max false node value: ', np.max(local_last_pred_y_false))
+    print('Min false node value: ', np.min(local_last_pred_y_false))
+    print('Max true node value: ', np.max(local_last_pred_y_true))
+    print('Min true node value: ', np.min(local_last_pred_y_true))
+    print('Max false edge value: ', np.max(local_last_pred_edge_false))
+    print('Min false edge value: ', np.min(local_last_pred_edge_false))
+    print('Max true edge value: ', np.max(local_last_pred_edge_true))
+    print('Min true edge value: ', np.min(local_last_pred_edge_true))
+    # normalized histogram
+    plt.hist(local_last_pred_y_true,  bins=50, label='Node True',  range=(0,1), ec='#04B5BBEE', lw=2, histtype='step')
+    plt.hist(local_last_pred_y_false, bins=50, label='Node False', range=(0,1), ec='#DF0345EE', lw=2, histtype='step')
+    plt.hist(local_last_pred_edge_true, bins=50, label='Edge True', range=(0,1), ec='#1C43C3EE', lw=2, histtype='step')
+    plt.hist(local_last_pred_edge_false, bins=50, label='Edge False', range=(0,1), ec='#BB8104EE', lw=2, histtype='step')
+
+    # log y axis
+    plt.yscale('log')
+    plt.xlabel('Score')
+    plt.ylabel('Count')
+    plt.xlim(0, 1)
+    plt.legend(loc='upper center')
+    if enable_save:
+        plt.savefig('./Node_Edge_Classification/pics/node_hist' + str(local_time.tm_year) + str(local_time.tm_mon) + str(local_time.tm_mday) + str(local_time.tm_hour) + str(local_time.tm_min) + str(local_time.tm_sec) + '.png')
+    else:
+        plt.show()
+
+    
+
 
